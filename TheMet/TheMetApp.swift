16ce59ead9a4ec6cbab47bff0231206c79a0e238
Ryan Westhoelter <jwesth1@live.spcollege.edu>
2024-09-22 19:32:22 -0400

    Initial Commit

---
+++ TheMet/TheMetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetApp.swift
+//  TheMet
+//
+//  Created by Ryan Westhoelter on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheMetTests/TheMetTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetTests.swift
+//  TheMetTests
+//
+//  Created by Ryan Westhoelter on 9/22/24.
+//
+
+import Testing
+@testable import TheMet
+
+struct TheMetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
